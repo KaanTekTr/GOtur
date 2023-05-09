60603daca3d9915b0ef6bc1d687950f93cb4578a
HP <alperengozeten@gmail.com>
2023-05-09 16:05:04 +0300

    backend start

---
+++ backend/GOtur/src/main/java/com/micra/GOtur/GOturApplication.java
@@ -0,0 +1,13 @@
+package com.micra.GOtur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GOturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GOturApplication.class, args);
+	}
+
+}

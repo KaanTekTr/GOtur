4d191e0a8d8adf5444cb1b70cad0d513ba36c5b8
HP <alperengozeten@gmail.com>
2023-05-25 15:34:10 +0300

    InitializerController

--- backend/GOtur/src/main/java/com/micra/GOtur/controllers/InitializerController.java
+++ backend/GOtur/src/main/java/com/micra/GOtur/controllers/InitializerController.java
@@ -1,9 +1,31 @@
 package com.micra.GOtur.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/initialize")
 @CrossOrigin
 public class InitializerController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public InitializerController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping("/initializeDB")
+    public ResponseEntity<String> createDB() {
+        jdbcTemplate.execute("DROP DATABASE IF EXISTS gotur");
+        jdbcTemplate.execute("CREATE DATABASE gotur CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+
+        return new ResponseEntity<>("Successfully Initialized The Database", HttpStatus.OK);
+    }
 }

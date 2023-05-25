2279d74533e553e982a959cf35533e6c7c9627f9
HP <alperengozeten@gmail.com>
2023-05-25 15:15:58 +0300

    mysql server

---
+++ backend/GOtur/src/main/java/com/micra/GOtur/controllers/InitializerController.java
@@ -0,0 +1,9 @@
+package com.micra.GOtur.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class InitializerController {
+}

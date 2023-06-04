559b66b51c1955e6601048a1e0e7760491d39d16
Kürşad Güzelkaya <55952069+kursadguzelkaya0@users.noreply.github.com>
2023-06-04 14:26:12 +0300

    add favorite restorant

--- frontend/src/components/UI/restaurant/RestaurantCard.jsx
+++ frontend/src/components/UI/restaurant/RestaurantCard.jsx
@@ -5,19 +5,28 @@ import "../../../styles/product-card.css";
 import { Link, useNavigate } from "react-router-dom";
 
 import image01 from "../../../assets/images/dominos.png";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavRestaurantsThunk } from "../../../store/restaurant/restaurantSlice";
 
 const RestaurantCard = (props) => {
   const { restaurant_id, restaurant_name, min_delivery_price } = props.item;
 
+  const userId = useSelector(state => state.auth.userId);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const seeFoods = () => {
     navigate(`/restaurants/${restaurant_id}`)
   };
 
+  const addFavorite = () => {
+   dispatch(addFavRestaurantsThunk({userId, rest_id: restaurant_id}))
+  };
+
   return (
-    <div className="product__item" onClick={seeFoods}>
-      <div className="product__img">
+    <div className="product__item" >
+      <div className="product__img" onClick={seeFoods}>
         <img src={image01} alt="product-img" className="w-50" />
       </div>
 
@@ -29,6 +38,9 @@ const RestaurantCard = (props) => {
           <span className="min__price">Min delivery price: ${min_delivery_price}</span>
           <button  className="addTOCart__btn" onClick={seeFoods}>
             See Foods
+          </button> {"   "}
+          <button  className="addTOCart__btn" onClick={addFavorite}>
+            Favorite
           </button>
         </div>
       </div>
